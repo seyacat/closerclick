072cdedc356e8a6fef93adc3578d8696bda90c4a
Seyacat <seyacat@gmail.com>
2025-11-05 20:51:16 -0500

    Init Api

---
+++ api/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  // Configurar carpeta pública
+  app.useStaticAssets(join(__dirname, 'public'), {
+    prefix: '/public/',
+  });
+  
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
