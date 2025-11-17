fddccb3e29aae4f215dfd9d5833784bd40ea361e
Seyacat <seyacat@gmail.com>
2025-11-16 19:54:07 -0500

    Fix typescript errors

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebSocketModule } from './websocket/websocket.module';
 import { ProxyModule } from './proxy/proxy.module';
+import { GitModule } from './git/git.module';
 
 @Module({
-  imports: [WebSocketModule, ProxyModule],
+  imports: [WebSocketModule, ProxyModule, GitModule],
   controllers: [AppController],
   providers: [AppService],
 })
